9f589f52715946e7f624420b975378f19bad29e4
chanan-hash <67066282+chanan-hash@users.noreply.github.com>
2023-04-27 15:53:01 +0300

    Implementing Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,77 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Put your code here. 
+//Listening if the key is pressed
+(PRESSED)
+    @SCREEN
+    D=A
+
+    @i
+    M=D
+
+    @KBD
+    D=M
+
+    @FILL
+    D;JGT
+
+    @PRESSED
+    0;JMP
+
+(STOP_PRESS)
+    @SCREEN
+    D=A
+
+    @i
+    M=D
+
+    @KBD
+    D=M
+
+    @CLEAR_SCREEN
+    D;JEQ
+
+    @STOP_PRESS
+    0;JMP
+
+
+(FILL)
+    (BLACK)
+    @i
+    A=M
+    M=-1 // this is how we color in black, because of the Two's complement
+
+    @i
+    M=M+1
+
+    @KBD
+    D=A
+    @i
+    D=D-M
+
+    @BLACK
+    D;JGT
+
+@STOP_PRESS
+0;JMP
+
+(CLEAR_SCREEN)
+    (WHITE)
+        @i
+        A=M
+        M=0 // means white
+
+        @i
+        M=M+1
+
+        @KBD
+        D=A
+        @i
+        D=D-M
+
+        @WHITE
+        D;JGT
+
+@PRESSED
+0;JMP
